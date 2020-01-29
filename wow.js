b77fec526e69d1a972b3aa3e587e9d9efab41df1
MKassandra <58279074+MKassandra@users.noreply.github.com>
2020-01-29 14:13:26 +0800

    service page update

--- wow.js
+++ wow.js
@@ -1,1 +1,10 @@
-new WOW().init();
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 400) {
+      $(this).addClass("slide");
+    }
+  });
+});
